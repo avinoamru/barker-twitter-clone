d2dee26dfdd3a918cac483eaad7a0d6ecd61a597
Avinoam Ruach <ruachavinoam@gmail.com>
2020-09-19 21:31:29 +0300

    init

---
+++ barkRoute.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const monk = require("monk");
+const joi = require("joi");
+const db = monk(process.env.MONGO_URI);
+const barksCollection = db.get("barks");
+
+router.get("/", async (req, res, next) => {
+    try{
+        const barks = await barksCollection.find();
+        return res.json(barks)
+
+    }catch(err){
+        next(err)
+    }
+
+});
+
+
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const morgan = require('morgan');
+require('dotenv').config();
+const barkRoute = require('./barkRoute');
+
+app.use(cors())
+app.use(morgan("common"))
+app.use(express.json())
+
+app.use("/", barkRoute)
+
+
+
+
+
+
+
+const port = process.env.PORT || 9090;
+app.listen(port, () => {
+    console.log(`Connected to server on port: ${port}`)
+})
